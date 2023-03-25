aa394ceee725aa12d9a2a8104aa40f888ab668ba
Ravindra Farkade <farkaderravindra@gmail.com>
2023-03-25 22:41:05 +0530

    Increment and decrement program basic program

---
+++ Increment_decrement.java
@@ -0,0 +1,15 @@
+class Increment_Decrement {
+  public static void main(String[] args) {
+    int x = 10;
+    System.out.println(x);
+    int a = ++x;
+    int b = x++;
+    int c = x--;
+    int d = --x;
+
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+  }
+}
