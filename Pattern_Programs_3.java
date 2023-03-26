da1f6f274d0efa9a48d49773a11270ec1a5454c5
Ravindra Farkade <farkaderravindra@gmail.com>
2023-03-26 10:42:06 +0530

    updated content

---
+++ Pattern_Programs_3.java
@@ -0,0 +1,10 @@
+class Pattern_Programs_3 {
+  public static void main(String[] args) {
+    int i, j;
+    for (i = 1; i <= 5; i++) {
+      for (j = 1; j <= 5; j++) {
+        System.out.println("#");
+      }
+    }
+  }
+}
